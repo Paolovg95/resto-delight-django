aed8b19b77af705bcbe1023c85b299b5c11cc7b0
Bruno Ortega <brunoortega@Brunos-MacBook-Pro.local>
2022-06-29 17:27:30 -0400

    Chart JS setup - Dashboard

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,8 @@
+from multiprocessing import context
 import re
 from django.shortcuts import render
+from menus.models import Category
+import pandas as pd
 
 # Create your views here.
 def home_view(request):
@@ -11,5 +14,10 @@ def about_view(request):
     return render(request, 'about.html', {})
 
 def dashboard_admin(request):
-
-    return render(request, 'admin_dashboard.html', {})
+    category = Category.objects.all().values() #  Return Query Set
+    data = pd.DataFrame(category)
+    list = data.name.tolist() # DataFrame Column 'Name' to list
+    context = {
+        'object': list
+    }
+    return render(request, 'admin_dashboard.html', context)
